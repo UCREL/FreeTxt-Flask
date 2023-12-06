be16665f62fc8dc049e8a3133efa4622d2346212
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-12-06 17:01:26 +0000

    Update Summariser.py

--- Summariser.py
+++ Summariser.py
@@ -1,9 +1,31 @@
-from nltk import word_tokenize,sent_tokenize
+from nltk import sent_tokenize
 from summa.summarizer import summarize as summa_summarizer
+
 def text_rank_summarize(article, ratio):
-  return summa_summarizer(article, ratio=ratio)
+    """
+    Summarize the given article using the TextRank algorithm.
+
+    Parameters:
+    article (str): The text to be summarized.
+    ratio (float): A number between 0 and 1 that determines the proportion of the number of sentences in the original text to be included in the summary.
+
+    Returns:
+    str: The summarized text.
+    """
+    return summa_summarizer(article, ratio=ratio)
 
 def run_summarizer(input_text, chosen_ratio, lang='en'):
+    """
+    Summarizes the input text using the TextRank algorithm, with a fallback to the first sentence if the summary is empty.
+
+    Parameters:
+    input_text (str or iterable): The text to be summarized. If it's not a string, it will be converted to one.
+    chosen_ratio (float): A number between 0 and 1 that determines the proportion of the original text to be included in the summary. It's capped at a minimum of 0.1.
+    lang (str, optional): The language code for the text (default is 'en' for English).
+
+    Returns:
+    str: The summary of the input text or the first sentence if the summary is empty.
+    """
     # Convert input_text to a string if it isn't already
     if not isinstance(input_text, str):
         input_text = ' '.join(map(str, input_text))
