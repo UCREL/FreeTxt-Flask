3f6914757a4dc8bef889e4f3d7e888d8c6d72456
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2024-02-29 17:17:47 +0000

    Update script.js

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -912,7 +912,7 @@ function sendSelectedSentences() {
 
 
 let currentPagesent = 1;
-let itemsPerPagesent = 10; // or whatever number you choose
+let itemsPerPagesent = 10; // or whatever number 
 let totalPagessent = 1; // This will be calculated based on data length
 
 // Function to display the overall sentiment score and its explanation
@@ -1155,7 +1155,7 @@ function setupSelectionListener(elementId) {
         if (eventData) {
             const selectedData = [];
             eventData.points.forEach(point => {
-                // Ensure these mappings match your data structure
+                // Ensure these mappings match  data structure
                 if (point.y === 'negative') {
                     const datum = {
                         Review: point.x,          // Assuming point.x corresponds to 'Review'
@@ -1166,7 +1166,7 @@ function setupSelectionListener(elementId) {
                 }
             });
 
-            // Now call your displaySentimentTable function with the selected data
+            // Now call  displaySentimentTable function with the selected data
             displaySentimentTable(selectedData);
         }
     });
@@ -1202,7 +1202,7 @@ function handleWordTreeData(wordTreeData, search_word) {
         // Force a redraw after a slight delay
         setTimeout(() => {
             drawWordTree(currentWordTreeData, currentSearchWord);
-        }, 200); // You can adjust this delay if needed
+        }, 200); //adjust this delay if needed
     });
 }
 
@@ -2119,7 +2119,7 @@ function displayResults(data) {
     // Check for an error message in the response data
     if (data.error) {
         // Display the error message
-        $("#errorContainer").text(data.error); // Assuming you have a container to display errors
+        $("#errorContainer").text(data.error); 
         $("#errorContainer").removeClass("hidden"); // Show the error container
         alert('No instances found. Please choose another category as the data does not have this category.');
     } else {
@@ -2525,7 +2525,7 @@ let gridOptions = {
         params.api.deselectAll();
         selectDisplayedRows();
     },onFirstDataRendered: function() {
-        // Call your function here
+       
         setTimeout(() => {
             getSelectedColumns();
         }, 2000);
@@ -2576,7 +2576,7 @@ let gridOptions = {
                     }
                 },
                 inRangeInclusive: true,
-                browserDatePicker: true,  // If you want to use the browser's date picker
+                browserDatePicker: true,  
                 clearButton: true         // For resetting the filter
             }
         }
@@ -2899,7 +2899,7 @@ function getSelectedColumns() {
 
 
 function getMinMaxDatesFromGrid(columnName) {
-    // Assuming gridOptions is your grid configuration
+   
     const allDates = gridOptions.api.getColumnValues(columnName);
     const validDates = allDates.filter(date => moment(date, 'DD/MM/YYYY').isValid());
     return {
@@ -3154,7 +3154,7 @@ async function handleCategoryChange() {
             option.value = tag;
             option.text = tag;
             option.setAttribute('data-lang-en', tag);
-            option.setAttribute('data-lang-cy', tag); // Assuming you have the same value for both languages, adjust if needed
+            option.setAttribute('data-lang-cy', tag); 
             option.setAttribute('data-type', 'semtag');
             subCategoryDropdown.appendChild(option);
         });
@@ -3249,7 +3249,7 @@ function setLanguage(language) {
 
     
 
-    // Save other necessary state as per your application's requirements
+   
 
     // Step 2: Update localeText based on the language
     gridOptions.localeText = language === 'cy' ? welshTranslations : {};
@@ -3308,7 +3308,7 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch('/submitfeedback', {
             method: 'POST',
             body: formData,
-            credentials: 'include', // If your setup requires credentials
+            credentials: 'include',
             headers: {
                 'Accept': 'application/json',
             }
